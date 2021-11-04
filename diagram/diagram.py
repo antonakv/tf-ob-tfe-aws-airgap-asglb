1b4968b71a887a8e91f8f008d97bf86457962f77
Anton Akulov <antonakv@users.noreply.github.com>
2021-11-04 14:48:48 +0100

    Adding README and diagram #1

--- diagram/diagram.py
+++ diagram/diagram.py
@@ -19,48 +19,55 @@ from diagrams.aws.compute import EC2Instance
 from diagrams.onprem.network import Internet
 
 graph_attr_diagram = {
-    "fontsize": "35"
+    "fontsize": "35",
+    "arrowsize": "1.5"
 }
 
 graph_attr_clusters = {
-    "fontsize": "25"
+    "fontsize": "27",
+    "arrowsize": "1.5"
 }
 
 graph_attr_nodes = {
-    "fontsize": "15"
+    "fontsize": "25",
+    "arrowsize": "1.5"
 }
 
-with Diagram("TFE Airgap ASG LB", show = False, direction="TB", graph_attr=graph_attr_diagram, node_attr=graph_attr_nodes):
+edge_attr_diagram = {
+    "fontsize": "25"
+}
+
+with Diagram("TFE Airgap ASG LB", filename="diagram", show = False, direction="TB", edge_attr=edge_attr_diagram, graph_attr=graph_attr_diagram, node_attr=graph_attr_nodes):
     internet1 = Internet("Internet")
     with Cluster("AWS", graph_attr=graph_attr_clusters):
-        s3_data = SimpleStorageServiceS3Bucket("Data")
-        s3_logs = SimpleStorageServiceS3Bucket("Logs")
-        s3_airgap = SimpleStorageServiceS3Bucket("Airgap")
-        route53 = Route53("Route53")
+        s3_data = SimpleStorageServiceS3Bucket(" \nData")
+        s3_logs = SimpleStorageServiceS3Bucket(" \nLogs")
+        s3_airgap = SimpleStorageServiceS3Bucket(" \nAirgap")
+        route53 = Route53(" \nRoute53")
         with Cluster("VPC", graph_attr=graph_attr_clusters):
             igw = InternetGateway("Internet Gateway")
-            rt_private_1 = RouteTable("aws5-private-1")
-            rt_public_1 = RouteTable("aws5-public-1")
-            rt_private_2 = RouteTable("aws5-private-2")
-            rt_public_2 = RouteTable("aws5-public-2")
-            subnet_private_1 = PrivateSubnet("aws5-private-1")
-            subnet_private_2 = PrivateSubnet("aws5-private-2")
-            subnet_public_1 = PublicSubnet("aws5-public-1")
-            subnet_public_2 = PublicSubnet("aws5-public-2")
-            alb = ALB("Application\n Load\n Balancer")
-            rdsdn = RDSPostgresqlInstance("Postgresql")
-            autoscale = AutoScaling("ASG")
+            rt_private_1 = RouteTable(" \n\n \nRoute\ntable\nprivate-1")
+            rt_public_1 = RouteTable(" \n\n \nRoute\ntable\npublic-1")
+            rt_private_2 = RouteTable(" \n\n \nRoute\ntable\nprivate-2")
+            rt_public_2 = RouteTable(" \n\n \nRoute\ntable\npublic-2")
+            subnet_private_1 = PrivateSubnet(" \n\n \nPrivate\nsubnet\nprivate-1")
+            subnet_private_2 = PrivateSubnet(" \n\n \nPrivate\nsubnet\nprivate-2")
+            subnet_public_1 = PublicSubnet(" \n\n \nPublic\nsubnet\npublic-1")
+            subnet_public_2 = PublicSubnet(" \n\n \nPublic\nsubnet\npublic-2")
+            alb = ALB(" \n\n \nApplication\n Load\n Balancer")
+            rdsdn = RDSPostgresqlInstance(" \nRDS Postgresql")
+            autoscale = AutoScaling(" \nASG")
             internet1 >> igw
             route53 >> alb
             with Cluster("Availability Zone 1", graph_attr=graph_attr_clusters):
-                ngw1 = NATGateway("NAT Gateway 1")
-                ec2_1 = EC2Instance("TFE Instance 1")
+                ngw1 = NATGateway(" \n\n \nNAT\n Gateway 1")
+                ec2_1 = EC2Instance(" \nTFE Instance 1")
                 with Cluster("Private subnet 1", graph_attr=graph_attr_clusters):
                     igw >> rt_private_1 >> subnet_private_1 >> ngw1 >> ec2_1 >> rdsdn
                 with Cluster("Public subnet 1", graph_attr=graph_attr_clusters):
                     igw >> rt_public_1 >> subnet_public_1 >> alb >> autoscale >> ec2_1 >> [s3_data, s3_logs, s3_airgap]
             with Cluster("Availability Zone 2", graph_attr=graph_attr_clusters):
-                ngw2 = NATGateway("NAT Gateway 2")
+                ngw2 = NATGateway(" \n\n \nNAT\n  Gateway 2")
                 ec2_2 = EC2Instance("TFE Instance 2")
                 with Cluster("Private subnet 2", graph_attr=graph_attr_clusters):
                     igw >> rt_private_2 >> subnet_private_2 >> ngw2 >> ec2_2 >> rdsdn
